173e5c8611ebb380fb057360bf8039219b389da3
ANKIT YADAV <yadavankit840@gmail.com>
2021-05-26 14:32:29 +0530

    web config

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// const path = {
+//     curviness: 1,
+//     autoRotate: true,
+//     values = [
+//         {x:100, y: -20}
+//     ]
+// } 
+
+// const tween = new TimelineLite();
+
+// tween.add(
+//     TweenLite.to(".top-section",1, {
+//         bezier: path,
+//         ease: Power1.easeInOut
+//     })
+// )
